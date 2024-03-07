c7fa571c8b171d9c5522512443ab4c643ad847ed
shaheda546 <116637702+shaheda546@users.noreply.github.com>
2024-03-07 11:34:33 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "21f3001073",
+  password: "",
   host: "localhost",
   port: 5432,
   database: "salesdb"
